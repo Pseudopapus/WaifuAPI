c1c4adb9a81b4f15e5faf260b8d53031686fd3f9
Maldonado Pablo Ignacio <164094204+McMaldo@users.noreply.github.com>
2024-06-04 23:20:52 -0300

    Generación de los archivos básicos y estructuración de la fuente de datos (json)

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+console.log("inicio");
+let total = 1;
+let group = 1;
+fetch('./src/data/data.json')
+.then(response => {return response.json()})
+.then(data => {
+  data.src.forEach(e=>{
+      e.waifus.forEach(w=>{
+        document.body.innerHTML +=/*html*/`
+        <div class="w c${group}">
+          ${total} ${w.name.nick}
+        </div>`;
+        total+=1;
+      })
+      group+=(e.id ? 1 : 0);
+  })
+})
