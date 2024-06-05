6653d23738c696f94cdcded715b3781da3504ce7
Maldonado Pablo Ignacio <164094204+McMaldo@users.noreply.github.com>
2024-06-05 18:41:06 -0300

    Reordenamiento de la API

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,12 @@ fetch('./src/data/data.json')
   data.src.forEach(e=>{
       e.waifus.forEach(w=>{
         document.body.innerHTML +=/*html*/`
-        <div class="w c${group}">
+        <div class="w c${(w.id==null ? "" : group)}">
           ${total} ${w.name.nick}
         </div>`;
         total+=1;
       })
       group+=(e.id ? 1 : 0);
+      group=(group>=7 ? 1 : group);
   })
 })
