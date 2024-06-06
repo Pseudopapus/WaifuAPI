b43aa6c8cd2e20f6f012b39d3fa667a6dd965b32
Maldonado Pablo Ignacio <164094204+McMaldo@users.noreply.github.com>
2024-06-05 23:24:15 -0300

    Updated OnePiece

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,23 @@
 console.log("inicio");
 let total = 1;
 let group = 1;
+let main = document.querySelector('main');
 fetch('./src/data/data.json')
 .then(response => {return response.json()})
 .then(data => {
   data.src.forEach(e=>{
-      e.waifus.forEach(w=>{
-        document.body.innerHTML +=/*html*/`
+    let section = document.createElement("section");
+    section.innerHTML+=`<h3>${e.name}</h3>`;
+    main.appendChild(section);
+    e.waifus.forEach(w=>{
+      if(w.id){
+        section.innerHTML +=/*html*/`
         <div class="w c${(w.id==null ? "" : group)}">
           ${total} ${w.name.nick}
         </div>`;
         total+=1;
-      })
-      group+=(e.id ? 1 : 0);
-      group=(group>=7 ? 1 : group);
+      }
+    })
+    group=(group>=7 ? 1 : (e.id ? group+1 : 0));
   })
 })
