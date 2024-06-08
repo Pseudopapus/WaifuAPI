f1f9001c6991e9d0a83f604b8f51cdc7a90bcd98
McMaldo <maldonado.ignacio.pablo@gmail.com>
2024-06-07 23:21:18 -0300

    Updated view, 1 src added

--- src/js/index.js
+++ src/js/index.js
@@ -7,17 +7,23 @@ fetch('./src/data/data.json')
 .then(data => {
   data.src.forEach(e=>{
     let section = document.createElement("section");
-    section.innerHTML+=`<h3>${e.name}</h3>`;
+    section.innerHTML+=`<h2>${e.name}</h2>`;
     main.appendChild(section);
     e.waifus.forEach(w=>{
       if(w.id){
         section.innerHTML +=/*html*/`
-        <div class="w c${(w.id==null ? "" : group)}">
-          ${total} ${w.name.nick}
-        </div>`;
+        <article class="w c${(w.id==null ? "" : group)}">
+          <div class="w-img">
+            <div class="name">
+              <h4 class="center">${w.name.nick}</h4>
+              <h5 class="center">${total}</h5>
+            </div>
+            ${w.name.jap ? `<div class="jap"><h4 class="center">${w.name.jap}</h4></div>` : ""}
+          </div>
+        </article>`;
         total+=1;
       }
     })
-    group=(group>=7 ? 1 : (e.id ? group+1 : 0));
+    group=(group>=6 ? 1 : (e.id ? group+1 : 0));
   })
 })
