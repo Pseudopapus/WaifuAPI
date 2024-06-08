2487233f52abe888566682394d67a8507a60e74b
McMaldo <maldonado.ignacio.pablo@gmail.com>
2024-06-08 17:12:56 -0300

    Added Images

--- src/js/index.js
+++ src/js/index.js
@@ -13,10 +13,13 @@ fetch('./src/data/data.json')
       if(w.id){
         section.innerHTML +=/*html*/`
         <article class="w c${(w.id==null ? "" : group)}">
-          <div class="w-img">
+          <div class="w-box">
             <div class="name">
               <h4 class="center">${w.name.nick}</h4>
               <h5 class="center">${total}</h5>
+            </div>
+            <div class="w-img">
+              <img src="./src/img/${w.img ? w.img : "notFound.png"}" alt="" class="${w.img ? 'found' : 'notFound'}" />
             </div>
             ${w.name.jap ? `<div class="jap"><h4 class="center">${w.name.jap}</h4></div>` : ""}
           </div>
